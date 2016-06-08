024eb3f88de0ed086b8f5ed8949cdd0caf488960
Taylor Simpson <starwarstay@aol.com>
2016-06-08 07:21:38 -0700

    Initial Commit

---
+++ biblos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  biblos
+//
+//  Created by Taylor Simpson on 6/8/16.
+//  Copyright © 2016 Taylor Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
